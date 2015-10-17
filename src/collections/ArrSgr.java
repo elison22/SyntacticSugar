ccf465bdf2b05e7a58e16d571dbcb64ae3c51681
elison22 <behatdude@gmail.com>
2015-10-17 10:23:52 -0600

    Initial commit

---
+++ src/collections/ArrSgr.java
@@ -0,0 +1,44 @@
+package collections;
+
+/**
+ * Created by brandt on 10/17/15.
+ */
+public class ArrSgr {
+
+    public static byte[] array(byte... vals) {
+        return vals;
+    }
+
+    public static short[] array(short... vals) {
+        return vals;
+    }
+
+    public static int[] array(int... vals) {
+        return vals;
+    }
+
+    public static long[] array(long... vals) {
+        return vals;
+    }
+
+    public static float[] array(float... vals) {
+        return vals;
+    }
+
+    public static double[] array(double... vals) {
+        return vals;
+    }
+
+    public static boolean[] array(boolean... vals) {
+        return vals;
+    }
+
+    public static char[] array(char... vals) {
+        return vals;
+    }
+
+    public static Object[] array(Object... vals) {
+        return vals;
+    }
+
+}
